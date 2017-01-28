a7b2397c5c0ee2ed3fc8a915b3ec52a110a58d8b
Meenakshi Mishra <mishra.me@husky.neu.edu>
2017-01-28 10:52:05 -0500

    version2 of crawler

--- assignment01/assignment01/crawler2.py
+++ assignment01/assignment01/crawler2.py
@@ -2,39 +2,61 @@ import urllib
 import HTMLParser
 import nltk
 from bs4 import BeautifulSoup as bs
-
-#save the seed page content in a variable
-
-depthLimit = 5
+import time
+#variables and their initialization
+depthLimit = 1
 currentLevel = 1
 pageCollectionLimit = 1000
-politenessFactor = 1  # in seconds
-crawled = []
-toBeCrawled = []
+sleep_time = 1  # in seconds
 seedUrl = "https://en.wikipedia.org/wiki/Sustainable_energy"
-seedPage = urllib.urlopen(seedUrl).read()
-file = open("D:/work/IR/assignment01/seedPage.txt", "w+")
-file.writelines("url :" + seedUrl + '\n' + seedPage)
-file.close()
+prefix = "https://en.wikipedia.org"
+crawledPages = []
+toBeCrawledPages = [seedUrl]
+pg_no = 0
+#the below function is used to extract raw html of the page to be saved
 
-# we saved the crawled page in the above lines and saved the raw html with its URL in a file.
-# next we create a soup of the same and try to extract valuable inforamtion like new links to crawl
-soup = bs(seedPage, 'html.parser')
-content = str(soup)
+def extract_html_page(url, page_number):
+    time.sleep(sleep_time)
+    page = urllib.urlopen(url).read()
+    file = open("D:/work/IR/assignment01/page_" + str(page_number) + ".txt", "w+")
+    file.writelines("url : " + str(url) + '\n' + page)
+    file.close()
+    return page
 
-def find_links():
-    child_links = []
+
+def extract_new_links(page):
+    soup = bs(page, 'html.parser')
     for tag in soup.find_all('a'):
         href = tag.get("href")
-        if href is not None and ":" not in href and "#" not in href:
-            child_links.append(tag.get("href"))
-
-    for a_link in child_links:
-        print a_link
-
+        if href is not None and ":" not in href and "#" not in href and "/wiki" in href:
+            final_url = prefix + href
+            if not crawledPages.__contains__(final_url):
+                toBeCrawledPages.append(final_url)
     return
 
+
+def inc():
+    global pg_no
+    pg_no += 1
+    return pg_no
+
+def crawl():
+
+    if crawledPages.__sizeof__() > pageCollectionLimit or currentLevel > depthLimit:
+        return
+    for url in toBeCrawledPages:
+        if url not in crawledPages:
+            pg_no = inc()
+            page = extract_html_page(url, pg_no)
+            extract_new_links(page)
+            toBeCrawledPages.remove(url)
+            crawledPages.append(url)
+    return
+
+
 def main():
-    find_links()
+    pg_no = 0
+    crawl()
+    return
 
 main()
