3711da1d8f79c380164a1387990f8735c2e21947
Meenakshi Mishra <mishra.me@husky.neu.edu>
2017-02-27 15:49:19 -0500

    added code for convergence
    this version has convergence of value

--- assignment02/PageRank/PageRank.py
+++ assignment02/PageRank/PageRank.py
@@ -3,18 +3,19 @@
 
 #populate pages in files if not done already
 from collections import OrderedDict
-all_pages = OrderedDict()
+import math
+all_pages = []
 in_links = OrderedDict()
 out_links = OrderedDict()
-sinks = OrderedDict
+sinks = []
 graph2 = "graph2" #the existing file for Wt2g links
 experiment = "experiment"
 input_file = experiment
 d = 0.85 #teleportation factor
 
 #function to load data into the dictionaries
-def load_data_in_dictionaries():
-    file = open(input_file, 'r')
+def load_data_in_dictionaries(graph):
+    file = open(graph, 'r')
     lines = file.readlines()
 
     #populating in_links
@@ -36,7 +37,6 @@ def load_data_in_dictionaries():
             key2 = 0
             out_link_values = []
             if value in out_links.keys():
-                print "entered the if on " + value
                 out_links.get(value).append(key)
             else:
                 key2 = value
@@ -46,7 +46,6 @@ def load_data_in_dictionaries():
     print out_links
 
     #populate sinks
-    sinks = []
     for key in in_links.keys():
         if (out_links.get(key) is None):
             sinks.append(key)
@@ -54,37 +53,58 @@ def load_data_in_dictionaries():
     print sinks
 
     #populate all pages
-    all_pages = []
     for key_in in in_links.keys():
         if(key_in not in all_pages):
             all_pages.append(key_in)
     for key_out in out_links.keys():
         if(key_out not in all_pages):
             all_pages.append(key_out)
+    print all_pages
+
+# //	P	is	the	set	of	all	pages;	|P|	=	N
+# //	S	is	the	set	of	sink	nodes,	i.e.,	pages	that	have	no	out	links
+# //	M(p)	is the	set	(without	duplicates)	of	pages	that	link	to	page	p
+# //	L(q)	is	the	number	of	out-links	(without	duplicates)	from	page	q
+# //	d	is	the	PageRank	damping/teleportation	factor;	use	d	=	0.85	as	a	fairly	typical
+
+
+def find_page_rank(graph):
+    load_data_in_dictionaries(graph)
+    page_rank = []
+    P = all_pages
+    N = len(all_pages)
+    for page in P:
+        page_rank[page] = 1/N
+
+    while (page_rank_converged(page_rank) is not True):
+        sink_page_rank = 0
+        for sink in sinks:
+            sink_page_rank += page_rank[sink]
+        for page in all_pages:
 
 
 
 
+def page_rank_converged(page_rank):
+    entropy = calculate_entropy(page_rank)
+    perplexity = 2^entropy
+    if (perplexity < 1):
+        return True
+    else:
+        return False
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+total = 0
+def calculate_entropy(page_rank):
+    for rank in page_rank:
+        total =+ rank*(math.log(rank, 2))
+    entropy_value = 0-total
+    return entropy_value
 
 
 
 def main():
-    load_data_in_dictionaries()
+    graph = input_file
+    find_page_rank(graph)
 
 main()
 
